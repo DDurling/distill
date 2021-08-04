a30adea967e8cf5fdfb8f7fae2644fb2a563259e
Daniel Durling <danieldurling@gmail.com>
2021-08-04 16:28:47 +0100

    final website commit

--- script.R
+++ script.R
@@ -3,3 +3,8 @@
 library(distill)
 
 create_website(dir = ".", title = "iyo-distill", gh_pages = TRUE)
+
+postcards::create_postcard(template = "jolla", file = "about.Rmd") 
+
+usethis::use_github_pages(branch = "main", path = "/docs")
+
